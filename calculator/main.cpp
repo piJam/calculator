e4b465ba4d0a3ecd117ad25bb589c150b16af688
pijam <pijamly@gmail.com>
2020-02-12 11:01:04 +0800

    1

--- calculator/main.cpp
+++ calculator/main.cpp
@@ -1,11 +1,38 @@
-#include "widget.h"
 #include <QApplication>
+#include <QWidget>
+#include <QLineEdit>
+#include <QPushButton>
 
 int main(int argc, char *argv[])
 {
+    int ret = 0;
     QApplication a(argc, argv);
-    Widget w;
-    w.show();
+    QWidget* w = new QWidget(NULL, Qt::WindowCloseButtonHint);
 
-    return a.exec();
+    QLineEdit* le = new QLineEdit(w);
+
+    le->move(10, 10);
+    le->resize(240, 30);
+
+    QPushButton* button[20] = {0};
+
+    for(int i=0; i<4; i++)
+    {
+        for(int j=0; j<5; j++)
+        {
+            button[i*5 + j] = new QPushButton(w);
+            button[i*5 + j] -> resize(40, 40);
+            button[i*5 + j] -> move( 10 + (10 + 40) * j, 50 + (10 + 40) * i);
+        }
+    }
+
+
+
+    w ->show();
+
+    ret = a.exec();
+
+    delete w;
+
+    return ret;
 }
