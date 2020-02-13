78c76259d9d77e5816bf901e7d41bbb8a5607c0b
pijam <pijamly@gmail.com>
2020-02-13 12:50:17 +0800

    1
    1

--- calculator/main.cpp
+++ calculator/main.cpp
@@ -17,22 +17,22 @@ int main(int argc, char *argv[])
 
     QPushButton* button[20] = {0};
     const char* btnText[20] =
-       {
-           "7", "8", "9", "+", "(",
-           "4", "5", "6", "-", ")",
-           "1", "2", "3", "*", "<-",
-           "0", ".", "=", "/", "C",
-       };
+    {
+        "7", "8", "9", "+", "(",
+        "4", "5", "6", "-", ")",
+        "1", "2", "3", "*", "<-",
+        "0", ".", "=", "/", "C",
+    };
 
     for(int i=0; i<4; i++)
     {
-        for(int j=0; j<5; j++)
-        {
-            button[i*5 + j] = new QPushButton(w);;
-            button[i*5 + j] -> resize(40, 40);
-            button[i*5 + j] -> move( 10 + (10 + 40) * j, 50 + (10 + 40) * i);
-            button[i*5 + j] -> setText(btnText[i*5 + j]);
-        }
+         for(int j=0; j<5; j++)
+         {
+             button[i*5 + j] = new QPushButton(w);;
+             button[i*5 + j] -> resize(40, 40);
+             button[i*5 + j] -> move( 10 + (10 + 40) * j, 50 + (10 + 40) * i);
+             button[i*5 + j] -> setText(btnText[i*5 + j]);
+         }
     }
 
     w ->show();
